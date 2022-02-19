3ad100066e7f55bc18a6cfd2ae42e58a8eb34727
张敏 <zhangmin1@uino.com>
2022-02-19 11:10:06 +0800

    fix: linebreak style

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
 	plugins: ['react', '@typescript-eslint'],
 	rules: {
 		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
 	},
